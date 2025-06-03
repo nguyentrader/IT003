9f6304376027c2098dc18f85a6bfcd1ab9d3d51d
Tran Nguyen Le <63175927+nguyentrader@users.noreply.github.com>
2025-06-03 08:48:05 +0700

    Add files via upload

---
+++ Tree/BST-TimkiemX.cpp
@@ -0,0 +1,91 @@
+//                         
+//
+//       69696969                         69696969
+//    6969    696969                   696969    6969
+//  969    69  6969696               6969  6969     696
+// 969        696969696             696969696969     696
+//969        69696969696           6969696969696      696
+//696      9696969696969           969696969696       969
+// 696     696969696969             969696969        969
+//  696     696  96969      _=_      9696969  69    696
+//    9696    969696      q(-_-)p      696969    6969
+//       96969696         '_) (_`         69696969
+//          96            /__/  \            69
+//          69          _(<_   / )_          96
+//         6969        (__\_\_|_/__)        9696
+//
+//         Buddha shines, no bugs alive
+//         God Bless        Never Crash
+
+#include <iostream>
+using namespace std;
+
+struct TNODE {
+	int key;
+	TNODE* pLeft;
+	TNODE* pRight;
+};
+typedef TNODE* TREE;
+
+TNODE* CreateNode(int x) {
+    TNODE* p = new TNODE;
+    p->key = x;
+    p->pLeft = NULL;
+    p->pRight = NULL;
+    return p;
+}
+
+void InsertNode(TREE& t, int x) {
+    if (t == NULL) {
+        t = CreateNode(x);
+    } else {
+        if (x < t->key) {
+            InsertNode(t->pLeft, x);
+        } else if (x > t->key) {
+            InsertNode(t->pRight, x);
+        }
+    }
+}
+
+void CreateTree(TREE& t) {
+    int x;
+    while (cin >> x && x != -1) {
+        InsertNode(t, x);
+    }
+}
+
+bool searchNode(TREE t, int x) {
+    if (t == NULL) {
+        return false; // Not found
+    }
+    if (t->key == x) {
+        return true; // Found
+    } else if (x < t->key) {
+        return searchNode(t->pLeft, x); // Search in left subtree
+    } else {
+        return searchNode(t->pRight, x); // Search in right subtree
+    }
+}
+
+int main() {
+	TREE T; //hay: TNODE* T;
+	T = NULL; // Khoi tao cay T rong, or: CreateEmptyTree(T)
+	CreateTree(T);
+
+	int x;
+	cin >> x;
+	if(searchNode(T, x)) cout << "true";
+	else cout << "false";
+	return 0;
+}
+
+//          _.-/`)
+//         // / / )
+//      .=// / / / )
+//     //`/ / / / /
+//     // /     ` /
+//   ||         /
+//    \       /
+//     ))    .'
+//         //    /
+//         /
---
+++ Tree/BST-TongInternalNode.cpp
@@ -0,0 +1,82 @@
+//                         
+//
+//       69696969                         69696969
+//    6969    696969                   696969    6969
+//  969    69  6969696               6969  6969     696
+// 969        696969696             696969696969     696
+//969        69696969696           6969696969696      696
+//696      9696969696969           969696969696       969
+// 696     696969696969             969696969        969
+//  696     696  96969      _=_      9696969  69    696
+//    9696    969696      q(-_-)p      696969    6969
+//       96969696         '_) (_`         69696969
+//          96            /__/  \            69
+//          69          _(<_   / )_          96
+//         6969        (__\_\_|_/__)        9696
+//
+//         Buddha shines, no bugs alive
+//         God Bless        Never Crash
+
+
+#include <iostream>
+using namespace std;
+
+struct TNODE {
+	int key;
+	TNODE* pLeft;
+	TNODE* pRight;
+};
+typedef TNODE* TREE;
+
+void CreateNode(TREE& t, int x) {
+    TNODE* p = new TNODE;
+    p->key = x;
+    p->pLeft = NULL;
+    p->pRight = NULL;
+    t = p;
+}
+
+void InsertNode(TREE& t, int x) {
+    if (t == NULL) {
+        CreateNode(t, x);
+    } else {
+        if (x < t->key) {
+            InsertNode(t->pLeft, x);
+        } else if (x > t->key) {
+            InsertNode(t->pRight, x);
+        }
+    }
+}
+
+void CreateTree(TREE& t) {
+    int x;
+    while (cin >> x && x != -1) {
+        InsertNode(t, x);
+    }
+}
+
+int SumInternalNode(TREE t) {
+    if (t == NULL) return 0;
+    if (t->pLeft == NULL && t->pRight == NULL) return 0; // Leaf node
+    return t->key + SumInternalNode(t->pLeft) + SumInternalNode(t->pRight);
+}
+
+int main() {
+	TREE T; //hay: TNODE* T;
+	T = NULL; // Khoi tao cay T rong, or: CreateEmptyTree(T)
+	CreateTree(T);
+
+	cout << SumInternalNode(T);
+	return 0;
+}
+
+//          _.-/`)
+//         // / / )
+//      .=// / / / )
+//     //`/ / / / /
+//     // /     ` /
+//   ||         /
+//    \       /
+//     ))    .'
+//         //    /
+//         /
